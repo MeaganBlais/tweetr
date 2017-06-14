b352c87c6f03e699f7c51a62e4fe90c1ba011913
Meagan Blais <meaganblais@gmail.com>
2017-06-14 14:09:32 -0400

    adding renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,53 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+ // Fake data taken from tweets.json
+ var data = [
+   {
+     "user": {
+       "name": "Newton",
+       "avatars": {
+         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+       },
+       "handle": "@SirIsaac"
+     },
+     "content": {
+       "text": "If I have seen further it is by standing on the shoulders of giants"
+     },
+     "created_at": 1461116232227
+   },
+   {
+     "user": {
+       "name": "Descartes",
+       "avatars": {
+         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+       },
+       "handle": "@rd" },
+     "content": {
+       "text": "Je pense , donc je suis"
+     },
+     "created_at": 1461113959088
+   },
+   {
+     "user": {
+       "name": "Johann von Goethe",
+       "avatars": {
+         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+       },
+       "handle": "@johann49"
+     },
+     "content": {
+       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+     },
+     "created_at": 1461113796368
+   }
+ ];
 
  var tweetData = {
   "user": {
@@ -19,26 +66,10 @@
   },
   "created_at": 1461116232227
 }
-// =======================================
-// <article class="user-tweets">
-//   <header>
-//     <img src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png">
-//     <h1>Bill Fields</h1>
-//     <h2>@MrFields</h2>
-//   </header>
-//     <!-- <p>"test"</p> -->
-//   <footer>
-//     10 days
-//   </footer>
-// </article>
-// ===========================================
-
-
 
  function createTweetElement(tweetData) {
 
    //take a tweet object and return a tweet article
-
   let $html = `<article class = "user-tweets">
       <h1>${tweetData.user.name}</h1>
       <img src = "${tweetData.user.avatars.small}"/>
@@ -48,38 +79,26 @@
       </article>`
 
   return $html
-
-  // let newTweet = $('<article>').addClass('user-tweets');
-      //append text to paragraph
-  //  newTweet.append($('<h1>').text(tweetData.user.name));
-  //  newTweet.append($('<img>').attr("src", tweetData.user.avatars.small));
-  //  newTweet.append($('<h2>').text(tweetData.user.handle));
-  //  newTweet.append($('<p>').text(tweetData.content.text));
-  //  newTweet.append($('<footer>').text(tweetData.created_at));
-  //  return newTweet;
-
  }
+
  var $tweet = createTweetElement(tweetData);
 // createTweetElement(tweetData);
 
+// $(document).ready(function(){
+//   $('#tweets-container').append($tweet);
+// });
 
 $(document).ready(function(){
-
-  $('#tweets-container').append($tweet);
-
+function renderTweets(tweets) {
+  // $('#tweets-container').empty(); //removes all child nodes of the matched elements from DOM
+  // loops through tweets
+  for (tweet in tweets) {
+    // calls createTweetElement for each tweet
+    let tweetData = tweets[tweet];
+    let $tweet = createTweetElement(tweetData);
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').append($tweet)
+  }
+}
+renderTweets(data);
 });
-
-
-// function renderTweets(tweets) {
-//   // loops through tweets
-//     // calls createTweetElement for each tweet
-//     // takes return value and appends it to the tweets container
-// }
-//
-// function createTweetElement(tweet) {
-//   var $tweet = $('<article>').addClass('tweet');
-//   // ...
-//   return $tweet;
-// }
-//
-// renderTweets(data);
