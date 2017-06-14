c313f96ec454b87bcec8bf91bc92156071b065d3
Meagan Blais <meaganblais@gmail.com>
2017-06-13 20:48:01 -0400

    adding character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,32 @@
+const maxChar = 140;
+
+// this needs to be a let since it is being reassigned in the function
+let charRemaining = maxChar;
+
+function getRemChar(totChar) {
+  return maxChar - totChar;
+}
+
+
+function readyFn(jQuery) {
+    // Code to run when the document is ready.
+    $('.new-tweet').find('textarea').on('keypress', function(event) {
+      // wrapping this is jQuery syntax $() allows for use of jQuery functions
+      charRemaining = getRemChar($(this).val().length);
+
+      // this uses jQuery to tranverse up the DOM tree
+      let counter = $(this).siblings('.counter');
+
+      counter.text(charRemaining);
+
+      if (charRemaining < 0) {
+        counter.addClass('over-char-limit');
+      } else {
+        counter.removeClass('over-char-limit');
+      }
+
+      console.log(counter)
+    });
+}
+
+$(document).ready(readyFn);
