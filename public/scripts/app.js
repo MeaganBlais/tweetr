27775c03e87fd7a1f2e803cb50407d5cdf56c0bf
Meagan Blais <meaganblais@gmail.com>
2017-06-15 14:49:17 -0400

    adding form validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,23 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
 
 $(document).ready(function(){
   function createTweetElement(tweetData) {
 
     //add escape() to each element that is not being tested elsewhere
     //take a tweet object and return a tweet article
-   let $html = `<article class = "user-tweets">
-       <h1>${tweetData.user.name}</h1>
-       <img src = "${tweetData.user.avatars.small}"/>
-       <h2>${tweetData.user.handle}</h2>
-       <p>${escape(tweetData.content.text)}</p>
-       <footer>${tweetData.created_at}</footer>
-       </article>`
+    let html = `<article class = "user-tweets">
+    <h1>${tweetData.user.name}</h1>
+    <img src = "${tweetData.user.avatars.small}"/>
+    <h2>${tweetData.user.handle}</h2>
+    <p>${escape(tweetData.content.text)}</p>
+    <footer>${tweetData.created_at}</footer>
+    </article>`
 
-   return $html
+    return html
   }
 
   function renderTweets(tweets) {
@@ -39,30 +39,55 @@ $(document).ready(function(){
     return div.innerHTML;
   }
 
+  // form submit handler
   $('.tweet-form').on('submit', function(event) {
     // console.log('data', data);
     event.preventDefault()
 
-        console.log('Button clicked, performing ajax call ...', $(this).serialize());
-        $.ajax({
-          url: '/tweets',
-          method: 'POST',
-          data: $(this).serialize(),
-          success: function () {
-            console.log('Success: ');
-            loadTweets();
-          }
-        });
-      });
-    function loadTweets() {
-      // fetching tweets from the host page
+
+    // $.flash('This message');
+
+    function validateData(data) {
+      // if (text === null || "") {
+      // console.log('data', data)
+      if (data.length > 140) {
+        alert('tweet is too long');
+        return false;
+      } else if (data === null || data === '') {
+        alert('please enter a tweet');
+        return false;
+      }
+      return true;
+    }
+
+    let validTweet = validateData($(this).find('textarea').val());
+
+    if (validTweet) {
+      console.log('Button clicked, performing ajax call ...', $(this).serialize());
       $.ajax({
         url: '/tweets',
-        method: 'GET',
-        success: renderTweets
+        method: 'POST',
+        data: $(this).serialize(),
+        success: function () {
+          console.log('Success: ');
+          loadTweets();
+        }
       });
     }
-    loadTweets();
+  });
+
+
+  // this needs to have a listener ... on.click??
+  function loadTweets() {
+    // fetching tweets from the host page
+    // do not call the function ... simply pass it to the success param
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: renderTweets
+    });
+  }
+  loadTweets();
 
 
 });
